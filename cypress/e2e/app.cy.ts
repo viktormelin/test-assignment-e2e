07a2494fe9b423d93056ee2622a426e8c0b6fc09
dough <viktor.melin@me.com>
2023-02-09 21:34:18 +0100

    split api and mock test

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -3,7 +3,20 @@ beforeEach(() => {
 });
 
 describe('Test submit handler', () => {
-	it('should find movies on submit', () => {
+	it('should display movies on submit', () => {
+		cy.intercept('GET', 'http://omdbapi.com/*', { fixture: 'movies' }).as('intercepted');
+
+		cy.get('#searchText').type('Star Wars').should('have.value', 'Star Wars');
+		cy.get('#searchForm').submit();
+
+		cy.wait('@intercepted');
+
+		cy.get('#movie-container').children().should('have.length', 10);
+		cy.get('#movie-container').children().first().get('h3').contains('Star Wars: Episode IV - A New Hope');
+		cy.get('#movie-container').children().first().get('img').should('have.attr', 'src');
+	});
+
+	it('should fetch correct movies from API', () => {
 		cy.get('#searchText').type('Star Wars').should('have.value', 'Star Wars');
 		cy.get('#searchForm').submit();
 
@@ -11,39 +24,39 @@ describe('Test submit handler', () => {
 		cy.get('#movie-container').children().first().get('h3').contains('Star Wars: Episode IV - A New Hope');
 		cy.get('#movie-container').children().first().get('img').should('have.attr', 'src');
 	});
+});
 
-	describe('should test errors', () => {
-		it('should display error if input is empty', () => {
-			cy.intercept('GET', 'http://omdbapi.com/*', { body: [] }).as('intercepted');
+describe('Test errors', () => {
+	it('should display error if input is empty', () => {
+		cy.intercept('GET', 'http://omdbapi.com/*', { body: [] }).as('intercepted');
 
-			cy.get('#searchText').should('have.value', '');
-			cy.get('#searchForm').submit();
+		cy.get('#searchText').should('have.value', '');
+		cy.get('#searchForm').submit();
 
-			cy.wait('@intercepted');
+		cy.wait('@intercepted');
 
-			cy.get('body').contains('Inga sökresultat att visa');
-		});
+		cy.get('body').contains('Inga sökresultat att visa');
+	});
 
-		it('should display error if input value is not found', () => {
-			cy.intercept('GET', 'http://omdbapi.com/*', { body: [] }).as('intercepted');
+	it('should display error if input value is not found', () => {
+		cy.intercept('GET', 'http://omdbapi.com/*', { body: [] }).as('intercepted');
 
-			cy.get('#searchText').type("I don't exist").should('have.value', "I don't exist");
-			cy.get('#searchForm').submit();
+		cy.get('#searchText').type("I don't exist").should('have.value', "I don't exist");
+		cy.get('#searchForm').submit();
 
-			cy.wait('@intercepted');
+		cy.wait('@intercepted');
 
-			cy.get('body').contains('Inga sökresultat att visa');
-		});
+		cy.get('body').contains('Inga sökresultat att visa');
+	});
 
-		it('should display error if API request fails', () => {
-			cy.intercept('GET', 'http://omdbapi.com/*', { fixture: 'error' }).as('intercepted');
+	it('should display error if API request fails', () => {
+		cy.intercept('GET', 'http://omdbapi.com/*', { fixture: 'error' }).as('intercepted');
 
-			cy.get('#searchText').type('Star Wars').should('have.value', 'Star Wars');
-			cy.get('#searchForm').submit();
+		cy.get('#searchText').type('Star Wars').should('have.value', 'Star Wars');
+		cy.get('#searchForm').submit();
 
-			cy.wait('@intercepted');
+		cy.wait('@intercepted');
 
-			cy.get('body').contains('Inga sökresultat att visa');
-		});
+		cy.get('body').contains('Inga sökresultat att visa');
 	});
 });
